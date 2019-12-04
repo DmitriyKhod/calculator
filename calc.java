b7b9e1f467765719c47c6dd9ece132c50e60849f
DmitriyKhod <58495162+DmitriyKhod@users.noreply.github.com>
2019-12-04 04:31:05 +0300

    Create calc.java

---
+++ calc.java
@@ -0,0 +1,47 @@
+package calc;
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class calc {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+            Scanner scanner = new Scanner(System.in);
+            math math = new math();
+ 
+            while (true) {
+ 
+                System.out.print("Enter something : ");
+                String input = scanner.nextLine();
+ 
+                if ("q".equals(input)) {
+                    System.out.println("Exit!");
+                    break;
+                }
+ 
+                System.out.println("input : " + input);
+                System.out.println("-----------\n");
+                String[] arrOfStr = input.split("[ ]+");
+                
+                switch(arrOfStr[1])
+                {
+                   case "+" :
+                      System.out.println(math.Add(arrOfStr[0], arrOfStr[2])); 
+                      break;
+                   case "-" :
+                      System.out.println(math.Subtract(arrOfStr[0], arrOfStr[2])); 
+                      break;
+                   case "*" :
+                       System.out.println(math.Multiply(arrOfStr[0], arrOfStr[2])); 
+                       break;
+                   case "/" :
+                       System.out.println(math.Divide(arrOfStr[0], arrOfStr[2])); 
+                       break;
+                   default :
+                	   throw new ArithmeticException("Неверная операция");
+                }
+            }
+            scanner.close();
+    }
+}
+
