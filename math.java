d6227442574c003c8050af28b195a4f2bef21ebd
DmitriyKhod <58495162+DmitriyKhod@users.noreply.github.com>
2019-12-19 08:43:33 +0200

    Update math.java

--- math.java
+++ math.java
@@ -1,67 +1,31 @@
-package calc;
-import java.util.regex.*;  
-
 public class math {
-	public int Add (String a, String b)
-	{
-		int[] pair = checker(a,b);
-        return pair[0] + pair[1];
-	}
+   public static int rim_mode = 0;
+   public static String operation = ""; 
+   public static int rezult = 0;
+   public static int operand1 =0;
+   public static int operand2 =0;
 
-	public int Subtract (String a, String b)
-	{
-		int[] pair = checker(a,b);
-        return pair[0] - pair[1];
-	}
+   public task docalculation1(task task) {
+	   
+	   rim_mode  = task.rim_mode;
+       switch(task.operation)
+       {
+          case "+" :
+        	  task.rezult = task.operand1 + task.operand2;
+        	 return task;
+          case "-" :
+        	  task.rezult = task.operand1 - task.operand2; 
+        	  return task;
+          case "*" :
+        	  task.rezult = task.operand1 * task.operand2;
+        	  return task;
+          case "/" :
+        	  task.rezult = task.operand1 / task.operand2;
+        	  return task;
+          default :
+              throw new ArithmeticException("Неверная операция");
 
-	public int Multiply (String a, String b)
-	{
-		int[] pair = checker(a,b);
-        return pair[0] * pair[1];
-	}
+       }
+   }
 
-	public String Divide (String a, String b)
-	{
-		int[] pair = checker(a,b);
-		
-        float rez = (float) pair[0] / pair[1];
-        
-        return Float.toString(rez)  ;
-	}
-	
-	private static int[] checker(String a,String b){
-		int[] pair = {0,0};
-		
-		if(Pattern.matches("\\d+", a)&&Pattern.matches("\\d+", b) ){
-			pair[0] = Integer.parseInt(a);
-			pair[1] = Integer.parseInt(b);
-			if ((pair[0] >0)&&(pair[0] < 11)&&(pair[1] >0)&&(pair[1] < 11))
-			return pair;
-			else {throw new ArithmeticException("Неверная операция");}
-		}
-		
-		pair[0] = rimtodigit(a) ;
-		pair[1] = rimtodigit(b) ;
-		
-		return pair;
-	}
-	
-	private static int rimtodigit(String a){
-		
-        switch(a)
-        {
-           case "I" : return 1; 
-           case "II" : return 2; 
-           case "III" : return 3;
-           case "IV" : return 4; 
-           case "V" : return 5; 
-           case "VI" : return 6; 
-           case "VII" : return 7; 
-           case "VIII" : return 8; 
-           case "IX" : return 9; 
-           case "X" : return 10; 
-        }
-		
-        throw new ArithmeticException("Неверная операция");
-	}
 }
