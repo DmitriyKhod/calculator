372b0023b76759a18b0a27e1f485947260a74f9d
DmitriyKhod <58495162+DmitriyKhod@users.noreply.github.com>
2019-12-19 08:41:56 +0200

    Create task.java

---
+++ task.java
@@ -0,0 +1,16 @@
+public class task {
+	   public int rim_mode = 0;
+	   public String operation = ""; 
+	   public int rezult = 0;
+	   public int operand1 =0;
+	   public int operand2 =0;
+	   
+	   public task(int Rim_mode,String Operation,int Rezult ,int Operand1,int Operand2 ) {
+		   rim_mode = Rim_mode; 
+		   operation = Operation;
+		   rezult = Rezult;
+		   operand1 = Operand1;
+		   operand2 = Operand2;
+		  }
+	   public task() { }
+}
